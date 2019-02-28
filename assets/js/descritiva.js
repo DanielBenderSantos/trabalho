c9cfc362392fc46840f1220ec35f2360d6deaa3e
Daniel Bender <danielbendersantos@gmail.com>
2019-02-28 11:54:39 -0300

    Descritiva parte basica adicionada

---
+++ assets/js/descritiva.js
@@ -0,0 +1,4 @@
+function abrepg(link){
+	window.location.href=link//           ------   isso faz com que abra o link na msm pagina ----
+	//window.open(link);// isso faz com que abra o link em uma nova guia
+}
