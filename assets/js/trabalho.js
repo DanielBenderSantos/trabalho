0df96a6e3e2509f3e521e6038dbbaf2dd1009f0f
Daniel Bender <danielbendersantos@gmail.com>
2019-02-28 10:23:56 -0300

    atualização

---
+++ assets/js/trabalho.js
@@ -0,0 +1,7 @@
+		function abrepg(link){
+			window.location.href=link//           ------   isso faz com que abra o link na msm pagina ----
+			//window.open(link);// isso faz com que abra o link em uma nova guia
+		}
+		function menu(link){
+			window.location.href=link
+		}
