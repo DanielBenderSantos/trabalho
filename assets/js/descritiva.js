9bd0bbf8fab465fc5d77a627b4794f5c97ac2f22
Daniel Bender <Danielbendersantos@gmail.com>
2019-03-24 23:38:43 -0300

    atualização

--- assets/js/descritiva.js
+++ assets/js/descritiva.js
@@ -1,11 +1,68 @@
 var vet = [];
 var vet2 = [];
+var vet3 = [];
 var ordens = [];
 
+
+var nomeP = [];
+var fi =[];
+var fr =[];
+var fac =[];
+var facP =[];
+var corpo_tabela = document.querySelector("tboby");
+
+
+
+//function tabela(nomeP,fi,fr,fac,facP){
+//	this.nomeP = nomeP //= function(){
+//	//	return(document.getElementById("nomeP").value;)
+//  //	}
+//	this.fi =  function calculaFi(){
+//				var i =0 
+//				var j =0
+//       			var nVezes = 1
+//      		 	var moda  =[];
+//       			var comparaV = 0;
+//					    for (i = 0; i < vet3.length; i++) { //  percorre o vetor 1 vez
+//            				nVezes = 1; //isso zera a conta
+//           					for (var j = i + 1; j < vet3.length; j++) { //  percorre o vetor 2 vez
+//                				if (vet3[i] == vet3[j]) { // compara se o numero i existe no vetor 
+//                    				++nVezes; //isso acumula o tanto de vezes que i aparece no vetor
+//                    				for(i=0;i<vet3.length;i++){
+//                    					fi[i] = nVezes[i]
+//                    				}
+//              					}
+//            				}
+//        				}
+//		alert(fi)
+//		}
+//	this.fr = fr
+//	this.fac = fac
+//	this.facP = facP
+//
+//	this.criar_linha_na_tabela = function(){
+//		//Criar elementos
+//		var linha = document.createElement("tr");
+//		var nomeP = document.createElement("td");
+//		var fi = document.createElement("td");
+//		var fr= document.createElement("td");
+//		var fac = document.createElement("td");
+//		var facP = document.createElement("td");
+//		//aplicar um estilo CSS
+//		//------nao vou por------
+//		//criar nos
+//		fi.appendChild(fi)
+//		linha.appendChild(fi)
+//		corpo_tabela.appendChild(linha)
+//	}
+//
+//}
 
 function adiciona(){
 	var dadosN = document.getElementById("dadosN").value;
  	   vet.push(dadosN);
+ 	var pmedia  = Number(document.getElementById("dadosN").value);
+ 		vet3.push(pmedia)
 	    document.getElementById("saida").innerHTML = dadosN + " adicionado ";	         
 			//agora vai ordena
 	        var aux = 0;
@@ -22,6 +79,7 @@ function adiciona(){
     //ja esta ordenado
     localStorage.setItem("vetor", vet)  
 }
+
 
 function adicionaOrdem(){
 
@@ -53,7 +111,7 @@ function mostrarOrdem(){
 function media(){
 	var soma = 0
 	var media = 0 
-	numeros = vet
+	numeros = vet3
 	for(var i=0; i<numeros.length;i++){
 		soma = soma + numeros[i]
 	}
@@ -85,6 +143,7 @@ function voltarDescritiva(link){
 }
 function avancar(link){
 	media();
+//	tabela();
 	nomeP = document.getElementById("nomeP").value
 	localStorage.setItem("nomeVariavelDePesquisa", nomeP)
 	window.location.href=link// isso faz com que abra o link na msm pagina
@@ -121,3 +180,43 @@ function avancar(link){
 function voltar(link){
 		window.location.href=link// isso faz com que abra o link na msm pagina
 }
+
+
+//function moda(){
+//		var i =0 
+//		var j =0
+//        var nVezes = 1
+//        var moda  =[];
+//       var comparaV = 0;
+//        var vet = vet3 //vetor
+//        for (var i = 0; i < vet.length; i++) { //  percorre o vetor 1 vez
+//            nVezes = 1; //isso zera a conta
+//    for (var j = i + 1; j < vet.length; j++) { //  percorre o vetor 2 vez
+//         if (vet[i] == vet[j]) { // compara se o numero i existe no vetor 
+//              ++nVezes; //isso acumula o tanto de vezes que i aparece no vetor
+//           }
+//        }
+//         if(nVezes == comparaV){ // isso compara se a mais de uma moda
+//          	moda = vet[i] +"," + moda  // isso add as modas 
+//            
+//            }
+//           	if (nVezes > comparaV) { // isso compara se tanto de vezes que i apareceu foi maior que o maior ate agora
+//                moda = vet[i]; //isso faz com que o numero de i vire a moda
+//                comparaV = nVezes; // isso faz com que a camparação agora seja com o i que acaba de virar a moda 
+//            }
+//
+//
+//        }
+    //      if(moda.length == vet.length){ // isso compara se a quantidade de modas e igual a quantidades de numero 
+    //      	document.getElementById("saida2").innerHTML = ("Não tem moda") //isso diz que não se tem moda
+    //      }
+    //        else{
+//     		 	document.getElementById("saida2").innerHTML = ("Moda: "+ moda); // isso alerta a moda
+  	//		}
+    //  	alert(i)
+    //  	alert(j)
+    //		alert("moda.length = " + moda.length)
+    //		alert("vet.length = " + vet.length)
+     //		alert(moda[0])
+
+//}
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
 function avancar(link){
 	window.location.href=link
 }
+// placeholder="" isso faz com que apareça o que eu escrever dentro do input
